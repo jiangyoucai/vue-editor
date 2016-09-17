081ebc6801d309e68e5a63f4851e6c4b9ee0efb6
姜灏 <150haomengting@163.com>
2016-09-17 10:49:07 +0800

    update editor ,now we can use the editor as vue components

---
+++ Demo.vue
@@ -0,0 +1,36 @@
+<template lang="html">
+    <div class="">
+        <v-editor :input-content="inputContent" :output-content.sync="outputContent" :upload-url="uploadURL"></v-editor>
+        <input type="button" name="name" value="submit" @click="submit">
+    </div>
+</template>
+
+<script>
+import Editor from '../../components/manage/Editor'
+export default {
+    data() {
+        return {
+            // input content to editor
+            inputContent: 'base on wangeditor',
+            // output content from editor
+            outputContent: '',
+            // set image upload api url
+            uploadURL: '/api/v1/help/upload/wangEditorH5File'
+        }
+    },
+    computed: {},
+    ready() {},
+    attached() {},
+    methods: {
+        submit() {
+            console.log(this.outputContent)
+        }
+    },
+    components: {
+        'v-editor': Editor
+    }
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ Editor.vue
@@ -0,0 +1,52 @@
+<template lang="html">
+    <div id="editor">
+        {{{inputContent}}}
+    </div>
+</template>
+
+<script>
+import WangEditor from 'wangeditor'
+export default {
+    props: ['inputContent', 'outputContent', 'uploadURL'],
+    data() {
+        return {}
+    },
+    computed: {},
+    ready() {
+        this.createEditor()
+    },
+    attached() {},
+    methods: {
+        createEditor() {
+            const self = this
+            const editor = new WangEditor('editor')
+            editor.config.menus = ['source', '|', 'bold', 'underline', 'italic', 'strikethrough', 'eraser', 'forecolor', 'bgcolor', '|', 'quote', 'fontfamily', 'fontsize', 'head', 'unorderlist', 'orderlist', 'alignleft', 'aligncenter', 'alignright',
+                '|', 'link', 'unlink', 'table', 'img', 'video', 'insertcode', '|', 'undo', 'redo', 'fullscreen'
+            ]
+            editor.config.uploadImgUrl = this.uploadURL
+            editor.onchange = function() {
+                self.formatContent(this.$txt.html())
+            }
+            editor.create()
+        },
+        formatContent(content) {
+            // handle
+            // ...
+            this.outputContent = content
+        }
+    },
+    components: {}
+}
+</script>
+
+<style lang="css" scoped>
+
+    #editor{
+        height: 500px;
+    }
+    .wangEditor-container{
+        border-radius: 2px;
+        overflow: hidden;
+        border: 1px solid #CCC;
+    }
+</style>
